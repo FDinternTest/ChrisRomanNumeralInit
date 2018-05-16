e5aac91e8fc75c9926c7af101e659432e0285db7
Christopher Vega <Christopher.Vega@firstdata.com>
2018-05-16 13:58:14 -0400

    complete kata

--- RomanNumeralTest2/src/RomanNumerals.java
+++ RomanNumeralTest2/src/RomanNumerals.java
@@ -1,6 +1,23 @@
 public class RomanNumerals {
 
+    private static final int[] VALUES = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] SYMBOLS = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
     public static String arabicToRoman(int arabic) {
-        return "";
+        int remains = arabic;
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < VALUES.length; i++) {
+            remains = romanAppend(remains, VALUES[i], SYMBOLS[i], result);
+        }
+        return result.toString();
+    }
+
+    public static int romanAppend(int arabic, int value, String roman, StringBuilder builder){
+        int result = arabic;
+        while(result >= value){
+            builder.append(roman);
+            result -= value;
+        }
+        return result;
     }
 }
--- RomanNumeralTest2/src/RomanNumeralsTest.java
+++ RomanNumeralTest2/src/RomanNumeralsTest.java
@@ -3,5 +3,37 @@ import org.junit.Test;
 
 public class RomanNumeralsTest {
 
+    @Test
+    public void oneTwoThree(){
+        Assert.assertEquals("1", "I", RomanNumerals.arabicToRoman(1));
+        Assert.assertEquals("2", "II", RomanNumerals.arabicToRoman(2));
+        Assert.assertEquals("3", "III", RomanNumerals.arabicToRoman(3));
+    }
 
+    @Test
+    public void four() {
+        Assert.assertEquals("4", "IV", RomanNumerals.arabicToRoman(4));
+    }
+
+    @Test
+    public void five() {
+        Assert.assertEquals("5", "V", RomanNumerals.arabicToRoman(5));
+    }
+
+    @Test
+    public void sixSevenEight() {
+        Assert.assertEquals("6", "VI", RomanNumerals.arabicToRoman(6));
+        Assert.assertEquals("7", "VII", RomanNumerals.arabicToRoman(7));
+        Assert.assertEquals("8", "VIII", RomanNumerals.arabicToRoman(8));
+    }
+
+    @Test
+    public void nine() {
+        Assert.assertEquals("9", "IX", RomanNumerals.arabicToRoman(9));
+    }
+
+    @Test
+    public void ten() {
+        Assert.assertEquals("10", "X", RomanNumerals.arabicToRoman(10));
+    }
 }
